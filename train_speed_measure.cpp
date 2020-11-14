0d8a8799ca026bf79b200ec95da8cbc49deada75
Dominik Haeusser <haeusser-dominik@web.de>
2020-11-14 08:15:38 +0100

    improve stopwatch for testing

--- train_speed_measure.cpp
+++ train_speed_measure.cpp
@@ -10,19 +10,9 @@
 using namespace std;
 
 
-// HAUPTPROGRAMM 
-// HIER WERDEN SIE IHRE PROGRAMME SCHREIBEN
-
-int main(int argc, char *argv[])
+int board_init(int h, int foundDLL)
 {
-    int CardAddress;
-    int dataOut1, dataOut2;
-    int dataIn1, dataIn2;
-    int h;
-int foundDLL = 0;
-
-   //BOARD WIRD INITIALISIERT!!
-   
+	   
     h = init();
 	if (!h)
 	{
@@ -32,47 +22,67 @@ int foundDLL = 0;
 	else
 	{
 			printf("DLL not found\n");return 0;
-
 	}
+	
 	if (foundDLL)
 	{
 		h = OpenDevice(0);
 		if (h == 0)
 		{
-			printf("Card 0 opened" );
+			printf("Card 0 opened\n");
+			return foundDLL;
 		}
 		else
 		{
 			printf("\nCard 0 not opened" ); return 0;
 		}
 	}
+}
 
+bool capture_input(int input)
+{
+	bool capture = false;
+	do
+	{
+		capture = ReadDigitalChannel(input);
+		
+	} while(capture != true);
+}
 
+int main(int argc, char *argv[])
+{
+    int CardAddress;
+    int dataOut1, dataOut2;
+    int dataIn1, dataIn2;
+    int h;
+	int foundDLL = 0;
 
-// AB HIER STEHT IHR CODE!!
-
-
-	if (foundDLL)
-	
+	if (board_init(h, foundDLL)) //Function initialize the board
 	{   
 		time_t timer;
 		double t_start, t_stop, t_diff;
-		int user_input;
+		int sensor_channel;
+		unsigned int sleep_time = 1; //in seconds
+		
 		cout << endl << "Welcome to train-speed measure" << endl;
-		cout << "Press Enter something to start stopwatch" << endl;
-		cin >> user_input;
+		cout << "What Input-Channel to you want to use to detect the train? (1-4)" << endl;
+		cin >> sensor_channel;
+		
+		cout << "Press Input " << sensor_channel << " to start the stopwatch" << endl;
+		capture_input(sensor_channel); //waiting for sensor interaction
 		t_start = time(&timer);
-		cout << "Press Enter something again to stop stopwatch" << endl;
-		cin >> user_input;
+		sleep(sleep_time); //digital_input needs time to reset
+		
+		cout << "Press Input " << sensor_channel << " again to stop the stopwatch" << endl;
+		capture_input(sensor_channel); //waiting for sensor interaction
 		t_stop = time(&timer);
+		
 		t_diff = t_stop - t_start;
 		cout << t_diff << " s" << endl;
-		
-		 ResetCounter(2); 
+		 
 		 ClearAllDigital(); 
          CloseDevice();
          FreeLibrary(hDLL);
          //system("pause");
     }
-    
 }
