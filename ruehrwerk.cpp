f60f073c77a9d18f8c6433937961dc29a2ea8725
Dominik Haeusser <haeusser-dominik@web.de>
2021-01-18 20:12:30 +0100

    create ruehrwerk.cpp

---
+++ ruehrwerk.cpp
@@ -0,0 +1,62 @@
+#include <cstdlib>
+#include <windows.h>
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+#include<stdio.h>
+#include "k8055.cpp"
+#include<stdlib.h>
+
+using namespace std;
+
+
+int board_init(int h, int foundDLL)
+{
+	   
+    h = init();
+	if (!h)
+	{
+		printf("DLL found\n");
+		foundDLL = 1;
+	}
+	else
+	{
+			printf("DLL not found\n");return 0;
+	}
+	
+	if (foundDLL)
+	{
+		h = OpenDevice(0);
+		if (h == 0)
+		{
+			printf("Card 0 opened\n");
+			return foundDLL;
+		}
+		else
+		{
+			printf("\nCard 0 not opened" ); return 0;
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+    int CardAddress;
+    int dataOut1, dataOut2;
+    int dataIn1, dataIn2;
+    int h;
+	int foundDLL = 0;
+
+	if (board_init(h, foundDLL)) //Function initialize the board
+	{   
+		while(1 == 1)
+		{
+			printf("Test\n");
+		}
+		
+		ClearAllDigital(); 
+        CloseDevice();
+        FreeLibrary(hDLL);
+        //system("pause");
+    }
+}
